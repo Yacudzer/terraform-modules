1a7afd37b9079b377dd2c5f3d8c51d3970212340
Evgenii Iakubov <evgenii.iakubov@lightspeedhq.com>
2023-11-25 18:36:59 +0300

    gcp_bucket_object_content version added

--- gcp_bucket_object_content/version.tf
+++ gcp_bucket_object_content/version.tf
@@ -4,5 +4,17 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 5.7.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.23.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.11.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.1"
+    }
   }
 }
